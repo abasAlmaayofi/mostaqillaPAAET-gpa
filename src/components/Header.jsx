3bd677d2e9dbab9d2850beaca0a7147f35054a55
abasAlmaayofi <abasalmaayofi@gmail.com>
2024-07-20 18:51:45 +0300

    modifying sidebar elements size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,22 +37,22 @@ function Header() {
         </Link>
         <Link
           href="https://mostaqillapaaet.com/wp-content/uploads/2024/07/efbfbdefbfbdd8a5d986d8acd8a7d8b2d8a7d8aa-d8a7d984d982d8a7d8a6d985d8a9-d8a7d984d985d8b3d8aad982d984d8a9-d981d98a-d8a7d984d8aad8b7d8a8d98ad982d98aefbfbd.pdf"
-          className="text-white underline md:text-5xl text-3xl"
+          className="text-white underline text-5xl "
         >
           إنجازات ومطالبات القائمة{" "}
         </Link>
-        <Link href="#" className="text-white underline md:text-5xl text-3xl">
+        <Link href="#" className="text-white underline text-5xl ">
           حساب المعدل التراكمي
         </Link>
         <Link
           href="http://mostaqillapaaet.com/#generalInformation"
-          className="text-white underline md:text-5xl text-3xl"
+          className="text-white underline text-5xl "
         >
           ملفات عامة
         </Link>
         <Link
           href="https://mostaqillapaaet.com/guidelines-videos/"
-          className="text-white underline md:text-5xl text-3xl"
+          className="text-white underline text-5xl "
         >
           فيديوهات
         </Link>
