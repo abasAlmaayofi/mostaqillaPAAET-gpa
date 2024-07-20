e9371a54d60bb4deb1d2883702290141c1e5a80c
abasAlmaayofi <abasalmaayofi@gmail.com>
2024-07-20 16:52:05 +0300

    rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import GPA from "./components/gpa";
 import Slogan from "./components/Slogan";
 import GPACalculator from "./components/GPACalculator";
 
